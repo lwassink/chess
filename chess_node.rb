22dd27ded0540ed1a4fef0e0f4ad0d2241ee4445
Luke Wassink <lwassink@gmail.com>
2016-09-22 07:41:15 -0700

    add stalemate condition

---
+++ chess_node.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+#
+# Copyright (C) 2016 Luke Wassink <lwassink@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+require_relative 'board'
+
+class ChessNode
+  def initialize()
+    
+  end
+end
